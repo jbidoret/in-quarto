988e6c474b237fb8bfa0362484b5d723011fea87
Julien Bidoret <julienbidoret@gmail.com>
2014-03-06 12:27:45 +0100

    better initial layout (sources)

--- js/source.js
+++ js/source.js
@@ -15,15 +15,42 @@ var source = {
 
 		var $list = '<div id="list"></div>';
 		$body.append($list);
-		var href = ("list.php");
-		$('#list').load(href + " #container", function() {
+		
+		$('#list').load("list.php #container", function() {
 
 	    	var container = document.querySelector('#container');
 
+	    	for (var i = 0; i < Math.floor($body.width()/220); i++) {
+	    		$stamp = $('<p class="stamp"></p>');
+	    		$('#container').prepend($stamp);
+	    	};
+	    	
+	    	$('.stamp').each(function(i){
+	    		var height = Math.floor(Math.random()*80 + 10);
+	    		$(this).css({
+	    			'height': height,
+	    			'position':'absolute',
+	    			'width' : '220px',
+	    			'left': 220 * i + 'px'
+
+	    		});
+	    	});
+
+	    	$('.item').each(function(){
+	    		if(Math.random() > 0.8) {
+	    			$(this).addClass('w2');
+	    		};
+	    	});
+
 	    	imagesLoaded( container, function() {
 	    		$body.addClass('packed');
-				var pckry = new Packery( container, { itemSelector: '.item', gutter: 20 });
-				sourceFocused = $('#list .item:first-child');
+				var pckry = new Packery( container, {
+					"itemSelector": '.item', 
+					"stamp": ".stamp",
+					"gutter": 20,
+					"columnWidth": 200
+				});
+				sourceFocused = $('#list .item').first();
 				sourceFocusedSrc = sourceFocused.attr('data-source');
 		
 				sourceFocused.addClass('focused');
